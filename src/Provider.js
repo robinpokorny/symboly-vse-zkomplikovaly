d8a1e1e1ce9ee8895e9549e07f661613609f2e31
Robin Pokorny <rpokorny@newstore.com>
2019-05-07 17:40:33 +0200

    :zap: Add custom provider

---
+++ src/Provider.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Provider = ({ children, index, length }) => (
+  <React.Fragment>
+    {children}
+
+    <div
+      style={{
+        position: 'absolute',
+        width: '100vw',
+        bottom: '1em',
+        display: 'flex',
+        padding: '0 50px',
+        justifyContent: 'space-between'
+      }}
+    >
+      <span>Node.js Meetup #34</span>
+      <a style={{ color: '#790f5b', textDecoration: 'none' }} href="https://twitter.com/robinpokorny">
+        @robinpokorny
+      </a>
+    </div>
+
+    <div
+      style={{
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        bottom: 0
+      }}
+    >
+      <div
+        style={{
+          background: '#790f5b',
+          height: 3,
+          width: `${100 * (index + 1) / length}%`
+        }}
+      />
+    </div>
+  </React.Fragment>
+)
+
+export default Provider
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+import highlight from '@mdx-deck/themes/syntax-highlighter'
+import Provider from './Provider'
+
+export default {
+  ...highlight,
+  Provider
+}
