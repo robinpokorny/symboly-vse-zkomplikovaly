96c0991abcac92dc9e1d73ecb31f758502bd7983
Robin Pokorny <robin.pokorny@klarna.com>
2020-03-31 11:41:16 +0200

    :zap: Translate to Czech

--- src/Provider.js
+++ src/Provider.js
@@ -14,7 +14,7 @@ const Provider = ({ children, index, length }) => (
         justifyContent: "space-between"
       }}
     >
-      <span>Node.js Meetup #34</span>
+      <span>Apríloví { Frontendisti.cz }</span>
       <a
         style={{ color: "#f00", textDecoration: "none" }}
         href="https://twitter.com/robinpokorny"
